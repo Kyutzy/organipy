37a01ed8494de4b4dc80b5a43f7f7eebde174652
cezap <ccz9000@yahoo.com>
2023-06-12 20:14:01 -0300

    indo

---
+++ X.py
@@ -0,0 +1,22 @@
+from cryptography.fernet import Fernet
+T=None
+i=open
+G=print
+from base64 import b64encode,b64decode
+J=b64decode
+import json
+a=json.load
+def D(Z=T):
+ l=K()
+ N=b64decode(l)
+ y=Fernet(N)
+ return y.decrypt(Z).decode('utf8')
+def K():
+ with i(J(b'Li9jaGF2ZS5rZXk='),'rb')as B:
+  l=B.read()
+  l=l.decode('utf8')
+  return l.split('|')[1]
+def F(P):
+ with i(J(b'cGFpbi5qc29u'),'r')as B:
+  return D(a(B)[P])
+
